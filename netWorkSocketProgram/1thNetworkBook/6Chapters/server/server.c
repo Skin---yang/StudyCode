68e9363b2a8c8eaf4ac0a8ad3991fb1dc379ffe2
skin_yang <skin_yang@126.com>
2017-09-05 19:01:36 +0800

    change server.c

--- netWorkSocketProgram/1thNetworkBook/6Chapters/server/server.c
+++ netWorkSocketProgram/1thNetworkBook/6Chapters/server/server.c
@@ -1,99 +1,120 @@
 #include "../../comm/unp.h"
 
 // this is a base sever
-// if hava a client conncet and sent a data to the server. the server will use the data answer client.
-
-void sig_chld(int a)
-{
-    pid_t pid;
-    int stat;
-
-    //pid = wait(&stat);
-    //if have some process exit on time. need the waitpid
-
-    while((pid = waitpid(-1,&stat,WNOHANG)) > 0)
-        printf("this process end. pid : %d\n", pid);
-
-    return;
-}
-
-void str_echo(int transSocket)
-{
-    if(transSocket == -1)
-    {
-        err_quit("the socket is invalid");
-    }
-
-    char buf[MAXLINE];
-    char sendbuf[MAXLINE + 10];
-    int nData = 0;
-again:
-    bzero(buf, MAXLINE);
-    while((nData = read(transSocket, buf, MAXLINE)) > 0)
-    {
-        bzero(sendbuf, sizeof(sendbuf));
-
-        sprintf(sendbuf, "server: %s", buf);
-        write(transSocket, sendbuf, sizeof(sendbuf));
-        printf("client: %s", buf);
-    }
-
-    if(nData < 0 && errno == EINTR)
-        goto again;
-    else if(nData < 0)
-    {
-        err_sys("str_echo function read fuction err.");
-    }
-}
+// this server use select deal with the client connect.
 
 void ServerBase()
 {
-   int sevSocket; 
-   struct sockaddr_in serveraddr;
-   int  transSocket;
-   pid_t childpid;
+    int sevSocket, i;
+    struct sockaddr_in serveraddr;
+    int  transSocket, connSocket;
+    int nready, client[FD_SETSIZE];
+    fd_set   reset, allset;
+    int maxfd, maxi;
+    char buf[MAXLINE];
+    bzero(buf, MAXLINE);
 
-   //create
-   sevSocket = socket(AF_INET, SOCK_STREAM, 0);
-   if(sevSocket < 0)
-       err_quit("sevSocket fail\n");
+    //create
+    sevSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if(sevSocket < 0)
+        err_quit("sevSocket fail\n");
 
-   bzero(&serveraddr, sizeof(serveraddr));
-   serveraddr.sin_family = AF_INET;
-   serveraddr.sin_port = htons(SERV_PORT);  // if not use the htons function, the server will bind * port.
-   // INADDR_ANY mean is 0.0.0.0 because a server can have multiple networks cards.
-   serveraddr.sin_addr.s_addr = INADDR_ANY;
-  
-   if(bind(sevSocket, (const struct sockaddr*)&serveraddr, sizeof(serveraddr)) < 0)
-       err_quit("bind fail\n");
+    bzero(&serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
 
-   listen(sevSocket, LISTENQ);
+    // if not use the htons function, the server will bind * port.
+    serveraddr.sin_port = htons(SERV_PORT);     
 
-   // capture the child process exit signal
-   //signal(SIGCHLD, sig_chld);
-   while(1)
-   {
-       transSocket = accept(sevSocket, NULL, NULL);
-       if(transSocket < 0)
-       {
-           if(errno == EINTR)
-           {
-               continue;
-           }
-           else
-           {
-               err_sys("accept error.errno: %d\n", errno);
-           }
-       }
-       
-       // create a process deal with a client
-       if((childpid = fork()) == 0)
-       {
-            str_echo(transSocket);
-            exit(0);
-       }
-       close(transSocket);      // parent process close connected socket
-   }
+    // INADDR_ANY mean is 0.0.0.0 because a server can have multiple networks cards.
+    serveraddr.sin_addr.s_addr = INADDR_ANY;
+
+    if(bind(sevSocket, (const struct sockaddr*)&serveraddr, sizeof(serveraddr)) < 0)
+        err_quit("bind fail\n");
+
+    listen(sevSocket, LISTENQ);
+
+    maxfd = sevSocket;
+    maxi = -1; // the variable is mean client arrary maximum index.
+
+    // init
+    for(i = 0; i < FD_SETSIZE; ++i)
+    {
+        client[i] = -1;
+    }
+
+    FD_ZERO(&allset);
+    FD_SET(sevSocket, &allset);
+    while(1)
+    {
+        reset = allset;
+        nready = select(sevSocket, &reset, NULL, NULL, NULL);
+
+        if(nready == -1)
+            err_quit("function select fail.\n");
+        
+        if(FD_ISSET(sevSocket, &reset))
+        {
+            transSocket = accept(sevSocket, NULL, NULL);
+
+            if(nready == -1)
+            {
+                err_quit("function select fail.\n");    
+            }
+
+            for(i = 0; i < FD_SETSIZE; ++i)
+            {
+                if(client[i] < 0)
+                {
+                    client[i] = transSocket;
+                    break;
+                }
+            } 
+
+            if(i == FD_SETSIZE)
+            {
+                err_quit("not have enought descriptot");
+            }
+
+            FD_SET(transSocket, &allset);
+
+            if(i > maxi)
+                maxi = i;
+
+            // update the maximum
+            if(transSocket > maxfd)
+                maxfd = transSocket;
+
+            if(--nready <= 0)
+                continue;
+        }
+
+        // check all client for data
+        for(i = 0; i < maxi; ++i)
+        {
+            if((connSocket = client[i]) < 0)
+                continue;
+
+            if(FD_ISSET(connSocket, &reset))
+            {
+                if(read(connSocket, buf, MAXLINE) == 0)
+                {
+                    // close and clear the socket
+                    close(connSocket);
+                    FD_CLR(connSocket, &allset);
+                    client[i] = -1;
+                }
+                else
+                {
+                    //print the server data
+                    puts(buf);
+                }
+
+                // not have more socket
+                if(--nready <= 0)
+                    break;
+            }
+        }
+    }
 }
 
 
